062fe321c5d2eb6d470f155636e94e65972e0eb9
tinntran <tinn6w6@gmail.com>
2021-10-26 13:59:56 +0700

    database connection

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,5 +1,8 @@
-function home(req, res) {
-    res.send('bruh')
+const Post = require('../models/post')
+
+async function home(req, res) {
+    const posts = await Post.find()
+    res.json(posts)
 }
 
 module.exports = { home }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 require('dotenv').config()
+const path = require('path')
 const express = require('express')
 const routers = require('./routers')
+
 const db = require('./config/db')
 
 db.connect()
 
 const PORT = process.env.PORT
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, '../public')))
 
 routers(app)
 
