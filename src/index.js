f10b825ce79bb4d9a76f94aee3646047a04f7673
tinntran <tinn6w6@gmail.com>
2021-10-29 18:52:40 +0700

    pug installation

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, '../public')))
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'resources/views'))
 
 routers(app)
