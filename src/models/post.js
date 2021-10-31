3eacc45a362f8392b0a0e741b63c6ed6ff0ca484
tinntran <tinn6w6@gmail.com>
2021-10-31 12:36:32 +0700

    slug & update ui

--- src/models/post.js
+++ src/models/post.js
@@ -1,19 +1,16 @@
 const mongoose = require('mongoose')
-const marked = require('marked')
 const slugify = require('slugify')
-const createDompurify = require('dompurify')
-const { JSDOM } = require('jsdom')
-const dompurify = createDompurify(new JSDOM().window)
+const uuid = require('uuid')
 
 const schema = new mongoose.Schema({
-    title: { type: String, required: true},
+    title: { type: String, required: true },
     content: { type: String, required: true },
     slug: { type: String, required: true, unique: true },
 }, { timestamps: true })
 
 schema.pre('validate', function (next) {
     if (this.title) {
-        this.slug = slugify(this.title, { lower: true, strict: true})
+        this.slug = `${slugify(this.title, { lower: true, strict: true })}${uuid.v4()}`
     }
 
     next()
