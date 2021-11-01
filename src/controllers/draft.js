f166e2b723cdcbcc3e8421e90628d83b16cc15d6
tinntran <tinn6w6@gmail.com>
2021-11-01 11:09:13 +0700

    update draft CRUD

--- src/controllers/draft.js
+++ src/controllers/draft.js
@@ -1,10 +1,40 @@
 const Draft = require('../models/draft')
+const marked = require('marked')
 
 async function index(req, res) {
     const drafts = await Draft.find({}).sort({ createdAt: -1 })
     res.render('draft', { drafts })
 }
 
+async function view(req, res) {
+    const draft = await Draft.findOne({ slug: req.params.slug })
+    res.render('draft/draftView', { draft, htmlContent: marked(draft.content) })
+}
+
+async function edit(req, res) {
+    const draft = await Draft.findOne({ slug: req.params.slug })
+    res.render('draft/editDraft', { draft })
+}
+
+async function deleteDraft(req, res) {
+    await Draft.findOneAndDelete({ slug: req.params.slug })
+    res.redirect('/draft')
+}
+
+async function updateDraft(req, res) {
+    try {
+        await Draft.findOneAndUpdate({ slug: req.params.slug }, req.body)
+        res.redirect(`/draft/${req.params.slug}`)
+    } catch (err) {
+        console.error(err)
+        res.redirect('/')
+    }
+}
+
 module.exports = {
-    index
+    index,
+    view,
+    edit,
+    deleteDraft,
+    updateDraft,
 }
--- src/routers/draft.js
+++ src/routers/draft.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 const controller = require('../controllers/draft')
 
+router.delete('/:slug', controller.deleteDraft)
+router.put('/:slug', controller.updateDraft)
+router.get('/edit/:slug', controller.edit)
+router.get('/:slug', controller.view)
 router.get('/', controller.index)
 
 module.exports = router
