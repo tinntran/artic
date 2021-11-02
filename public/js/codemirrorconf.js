3f3e489f1431b384ea00186056372a1caacdbebe
tinntran <tinn6w6@gmail.com>
2021-11-02 13:46:28 +0700

    adding codemirror textarea in the forms

--- public/js/codemirrorconf.js
+++ public/js/codemirrorconf.js
@@ -1,5 +1,12 @@
-const editor = CodeMirror.fromTextArea(document.querySelector("textarea#content"), {
+const content = document.querySelector("textarea#content")
+const editor = CodeMirror.fromTextArea(content, {
     lineNumbers: true,
     lineWrapping: true,
     mode: "markdown",
+})
+
+editor.setSize(null, '24rem')
+
+editor.on('change', () => {
+    content.textContent = editor.getValue()
 })
